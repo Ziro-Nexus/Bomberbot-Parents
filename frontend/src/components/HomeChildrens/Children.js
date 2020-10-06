7d1e256ef19744e78591e166bdd1a01eddaeb6ef
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-10-06 17:10:08 -0500

    fixed styles for children

--- frontend/src/components/HomeChildrens/Children.js
+++ frontend/src/components/HomeChildrens/Children.js
@@ -11,7 +11,7 @@ class Children extends React.Component {
    constructor(props){
         super(props)
         this.data = {
-            name_childres: ['Hijo1', 'Hijo2']
+            name_childres: ['Hijo1', 'jdfhjjh', 'hdgfdhj']
         }
         this.columns = this.data.name_childres.length;
         this.class = ''
@@ -23,32 +23,34 @@ class Children extends React.Component {
 
         {this.columns > 2 ? this.class = "container-fluid style-main style-main-scrool justify-content-center" : this.class = "container-fluid style-main justify-content-center"}
         return (
+            
             <div className={this.class}>
-            <div className="row align-items-center d-flex flex-grap">
+                
+                <div className="row align-items-center d-flex flex-grap">
+                <div className='spaces' ></div>
 
+                    {this.data.name_childres.map((name, i) => {
+                        {this.columns != 1 ? this.columns = "col-12 col-sm-6 rounded d-flex justify-content-center"
+                        : this.columns = "col-12 col-sm-12 rounded d-flex justify-content-center"}
+                        return (
+                        
+                        <div className={this.columns} key={i}>
 
-                {this.data.name_childres.map((name, i) => {
-                    {this.columns != 1 ? this.columns = "col-12 col-sm-6 rounded d-flex justify-content-center"
-                    : this.columns = "col-12 col-sm-12 rounded d-flex justify-content-center"}
-                    return (
-                     
-                    <div className={this.columns} key={i}>
-
-                        <div className="member shadow p-3 mb-5 bg-light ">
-                            <div className="member-img ">
-                                <img src={Avatar2} alt="Avatar" />
-                            </div>
-                            <div className="member-info">
-                                <h4>Little programmer 1</h4>
-                                <span>{name}</span>
+                            <div className="member shadow p-3 mb-5 mt-5 bg-light ">
+                                <div className="member-img ">
+                                    <img src={Avatar2} alt="Avatar" />
+                                </div>
+                                <div className="member-info">
+                                    <h4>Little programmer 1</h4>
+                                    <span>{name}</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    )
-                })}
-                
-                
-           </div>
+                        )
+                    })}
+                    
+                    
+            </div>
         </div>
         )
     }
--- frontend/src/components/login/Navbar.js
+++ frontend/src/components/login/Navbar.js
@@ -11,34 +11,39 @@ import { Link } from 'react-router-dom';
 class Navbar extends React.Component {
   render() {
     return (
-    <div className="row d-flex style-header justify-content-between align-items-center bg-light">
+        
+            <div className="row d-flex style-header justify-content-between align-items-center bg-light">
 
-        <div className="col-12 col-sm-4 d-flex align-items-center">
-            <Link to="/index">
-                <img className="img-logo" src={Photo} alt="Logo Bomberbot" />
-            </Link>
-        </div>
+                <div className="col-12 col-sm-4 d-flex align-items-center justify-content-center">
+                    <Link to="/index">
+                        <img className="img-logo" src={Photo} alt="Logo Bomberbot" />
+                    </Link>
+                </div>
 
-        <div className="col-12 col-sm-4 d-flex justify-content-center">
-            <a href="/">
-                <img className="image-pais" src={Kingdow} alt="Reino Unido"/>
-            </a>
-            <a href="/">
-                <img className="image-pais" src={Netherlands} alt="Países bajos" />
-            </a>
-            <a href="/">
-                <img className="image-pais" src={Filand} alt="Suomi" />
-            </a>
-        </div>
+                <div className="col-12 col-sm-4 d-flex justify-content-center">
+                    <a href="/">
+                        <img className="image-pais" src={Kingdow} alt="Reino Unido"/>
+                    </a>
+                    <a href="/">
+                        <img className="image-pais" src={Netherlands} alt="Países bajos" />
+                    </a>
+                    <a href="/">
+                        <img className="image-pais" src={Filand} alt="Suomi" />
+                    </a>
+                </div>
 
-        <div className="col-12 col-sm-4 d-flex justify-content-end">
-            <a href="https://bomberbot.com/">
-            <div className="home-bb d-flex align-items-center justify-content-center">
-                Go at Home
-            </div>
-            </a>
+                <div className="col-12 col-sm-4 d-flex justify-content-end">
+                    <a href="https://bomberbot.com/">
+                    <div className="home-bb d-flex align-items-center justify-content-center">
+                        Go at Home
+                    </div>
+                    </a>
+                </div>
+             
+                {/*                 <div class="spaces">
+            
+                </div> */}
         </div>
-    </div> 
     
     );
   }
