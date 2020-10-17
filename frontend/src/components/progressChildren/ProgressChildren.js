ad0bd569d6696025c98a01ff420aef7709eb8c05
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-10-17 13:44:55 -0500

    fixed  content for more information and changed styles for page modal

--- frontend/src/components/progressChildren/ProgressChildren.js
+++ frontend/src/components/progressChildren/ProgressChildren.js
@@ -132,7 +132,7 @@ export default function ProgressChildren(props){
                                     data={project}
                                     hideModal={hideModal}/>
     
-                                  </div>
+                                </div>
                             </div>
                         </div>
                     </div>
