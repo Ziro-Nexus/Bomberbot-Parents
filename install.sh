d93afd330a03646b56de6a12ad02fbba2bf5f41a
Camilo <1611@holbertonschool.com>
2020-10-20 21:29:13 -0500

    updating script

--- install.sh
+++ install.sh
@@ -5,3 +5,12 @@ sudo apt install -y nodejs
 sudo apt install -y python3-pip
 sudo apt install -y npm
 
+# PIP dependencies
+sudo pip3 install -r python_modules.txt
+
+# NPM dependencies
+sudo npm install --prefix frontend/
+
+# Compile Webpack
+sudo npm run webpack --prefix frontend/
+
