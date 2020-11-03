5286fae1f3302a38e552627bdadee342de55edfd
Deiwin Monsalve <deiwin1987@gmail.com>
2020-11-03 09:04:07 -0500

    Fix problem the formart in python with pep8

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,6 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
--- backend/views.py
+++ backend/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-
 def index(request):
 
     return render(request, "index.html")
+
 
 def home(request):
 
